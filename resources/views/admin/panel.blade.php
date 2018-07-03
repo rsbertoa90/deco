838837c62a33d875969557db6bfa6ab4d519fb0f
Rodrigo <rsbertoa90@gmail.com>
2018-07-02 21:11:00 -0300

    titulo en panel

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.default')
 
 @section('content')
+<ul class="list-inline menu-left mb-0">
+        <li class="list-inline-item">
+            <button type="button" class="button-menu-mobile open-left waves-effect">
+                <i class="ion-navicon"></i>
+            </button>
+        </li>
+        <li class="hide-phone list-inline-item app-search">
+            <h3 class="page-title">Panel de administracion</h3>
+        </li>
+    </ul>
+
+    <div class="clearfix"></div>
+
 
 <div class="row d-flex flex-column mt-5" id="edi-tables">
     @include('admin.seminars.edi-table')
@@ -12,7 +25,7 @@
 @section('js')
     <script src="assets/plugins/tiny-editable/mindmup-editabletable.js"></script>
     <script src="assets/plugins/tiny-editable/numeric-input-example.js"></script>
-    
+
     {{-- <script>
         $('#table').editableTableWidget().numericInputExample().find('td:first').focus();
     </script> --}}
