b4be6afc548b98b1e1000eb5de94104c55ef962b
Rodrigo <rsbertoa90@gmail.com>
2018-07-10 18:23:39 -0300

    fbtest

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,5 +1,11 @@
-<div class="row">
-    <div class="col-4 offset-4">
-        <img class="rounded-circle" src="{{$user->avatar}}" alt="{{$user->name}}">
+@extends('layouts.default')
+
+@section('content')
+    
+    <div class="row">
+        <div class="col-4 offset-4">
+            <img class="img-responsive img-circle img-rounded img-thumbnail rounded-circle" src="{{$user->avatar}}" alt="{{$user->name}}">
+        </div>
     </div>
-</div>
+
+@endsection
