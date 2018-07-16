8843f8d49276a2efa6c7e17d50e91b6553b34c22
rsbertoa90 <rsbertoa90@gmail.com>
2018-07-16 17:01:22 -0300

    mp listener test

--- app/Http/Controllers/MercadpPagoController.php
+++ app/Http/Controllers/MercadpPagoController.php
@@ -70,7 +70,7 @@ class MercadoPagoController extends Controller
     public function loginRedirect(){}
         
     public function listener(){
-        $response = response ('ok',200)->header('Content-Type', 'text/plain');;
+        $response = response ('ok',200);
         return $response;
     }
 }
