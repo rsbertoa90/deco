68e1294bacaf73f29b8456d1a62c3ce5ca68e2e2
rsbertoa90 <rsbertoa90@gmail.com>
2018-07-21 20:33:32 -0300

    bug

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -127,7 +127,7 @@ class EventController extends Controller
 
     public function getOnlineEvents()
     {
-        return Event::with('seminar')->where('mode','online')->get();
+        return Event::with('seminar')->where('date','>=', now())->where('mode','online')->get();
     }
 
 }
